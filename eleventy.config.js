b8cf0fc09b7e7b43cc66bbd80549928c79793872
Jackie <jacklynnnnnn@proton.me>
2026-03-15 17:14:58 +0100

    feat/header-component (#12)
    Addresses #10
    
    Reviewed-on: https://codeberg.org/jackie/jdaumeyer/pulls/12
    Co-authored-by: Jackie <jacklynnnnnn@proton.me>
    Co-committed-by: Jackie <jacklynnnnnn@proton.me>

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,27 +2,27 @@ import pluginWebc from "@11ty/eleventy-plugin-webc";
 import { RenderPlugin } from "@11ty/eleventy";
 
 export default function(eleventyConfig) {
-	// Watch CSS files
-	eleventyConfig.addWatchTarget("css/**/*.css");
+    // Watch CSS files
+    eleventyConfig.addWatchTarget("css/**/*.css");
 
-	eleventyConfig.addBundle("css", {
-		toFileDirectory: "dist",
-		// Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
-		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
-		bundleHtmlContentFromSelector: "style",
-	});
-	
-	// Bundle <script> content and adds a {% js %} paired shortcode
-	eleventyConfig.addBundle("js", {
-		toFileDirectory: "dist",
-		// Add all <script> content to the `js` bundle (use <script eleventy:ignore> to opt-out)
-		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
-		bundleHtmlContentFromSelector: "script",
-	});
+    eleventyConfig.addBundle("css", {
+	toFileDirectory: "dist",
+	// Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
+	// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
+	bundleHtmlContentFromSelector: "style",
+    });
+    
+    // Bundle <script> content and adds a {% js %} paired shortcode
+    eleventyConfig.addBundle("js", {
+	toFileDirectory: "dist",
+	// Add all <script> content to the `js` bundle (use <script eleventy:ignore> to opt-out)
+	// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
+	bundleHtmlContentFromSelector: "script",
+    });
 
     eleventyConfig.addPassthroughCopy("**/*.png");
 
-	// eleventyConfig.addPlugin(pluginWebc, {
+    // eleventyConfig.addPlugin(pluginWebc, {
     //     components: "_components/**/*.webc",
     // });
     eleventyConfig.addPlugin(RenderPlugin);
@@ -32,23 +32,24 @@ export default function(eleventyConfig) {
     });
 
     eleventyConfig.addFilter('await', function (promise, callback, result) {
-        promise.then(result => {
-            callback(null, result);
-        }).catch(err => {
-            callback(err);
-        });
+	promise.then(result => {
+	    callback(null, result);
+	}).catch(err => {
+	    callback(err);
+	});
     }, true);
 
-	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "nav", "posts"].indexOf(tag) === -1);
-	});
+    eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+	const ignoredTags = ["all", "nav", "home", "posts"]
+	return (tags || []).filter(tag => ignoredTags.indexOf(tag) === -1);
+    });
 
-	eleventyConfig.addFilter("alphabetically", strings =>
-		(strings || []).sort((b, a) => b.localeCompare(a))
-	);
+    eleventyConfig.addFilter("alphabetically", strings =>
+	(strings || []).sort((b, a) => b.localeCompare(a))
+    );
 
-	eleventyConfig.addFilter("getKeys", target => {
-		return Object.keys(target);
-	});
+    eleventyConfig.addFilter("getKeys", target => {
+	return Object.keys(target);
+    });
 
 };
